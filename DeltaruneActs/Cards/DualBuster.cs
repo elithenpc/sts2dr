54e4bdbb393efbdb8b8813bb83501a92cc44a4d6
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-08 21:05:14 +0930

    Add DualBuster spell

---
+++ DeltaruneActs/Cards/DualBuster.cs
@@ -0,0 +1,16 @@
+using DeltaruneActs.TP;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace DeltaruneActs.Cards;
+
+public sealed class DualBuster : DeltaruneActsCard
+{
+    public DualBuster() : base(0, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy) { }
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Target is null || cardPlay.Target.IsDead) return;
+        await DamageCmd.Attack(100).FromCard(this, cardPlay).Targeting(cardPlay.Target).Execute(choiceContext);
+    }
+}

43b9978d91940c82f636462bfc263e7319ef4c5c
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-08 21:05:35 +0930

    Register full Deltarune spell set

--- DeltaruneActs/ModMain.cs
+++ DeltaruneActs/ModMain.cs
@@ -27,11 +27,28 @@ public partial class ModMain : Node
         ModelDb.Inject(typeof(Cards.Joke));
         ModelDb.Inject(typeof(Cards.Flirt));
         ModelDb.Inject(typeof(Cards.Spare));
-        ModelDb.Inject(typeof(Cards.HealPrayer));
-        ModelDb.Inject(typeof(Cards.RudeBuster));
-        ModelDb.Inject(typeof(Cards.Pacify));
-        ModelDb.Inject(typeof(Cards.RedBuster));
 
-        Logger.Info("Deltarune Acts loaded: ACT + TP + spell system online.");
+        ModelDb.Inject(typeof(Cards.HealPrayer));
+        ModelDb.Inject(typeof(Cards.Pacify));
+        ModelDb.Inject(typeof(Cards.RudeBuster));
+        ModelDb.Inject(typeof(Cards.RedBuster));
+        ModelDb.Inject(typeof(Cards.IceShock));
+        ModelDb.Inject(typeof(Cards.SnowGrave));
+        ModelDb.Inject(typeof(Cards.DualHeal));
+        ModelDb.Inject(typeof(Cards.ReviveSong));
+        ModelDb.Inject(typeof(Cards.SleepMist));
+        ModelDb.Inject(typeof(Cards.Scythemare));
+        ModelDb.Inject(typeof(Cards.RudeSword));
+        ModelDb.Inject(typeof(Cards.Healing));
+        ModelDb.Inject(typeof(Cards.OkayHeal));
+        ModelDb.Inject(typeof(Cards.BetterHeal));
+        ModelDb.Inject(typeof(Cards.UltraHeal));
+        ModelDb.Inject(typeof(Cards.UltimateHeal));
+        ModelDb.Inject(typeof(Cards.DualBuster));
+        ModelDb.Inject(typeof(Cards.LightUp));
+        ModelDb.Inject(typeof(Cards.WakeKris));
+        ModelDb.Inject(typeof(Cards.ReviveKris));
+
+        Logger.Info("Deltarune Acts loaded: ACT + TP + full spell set online.");
     }
 }

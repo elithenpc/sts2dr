3e6583c5f24101fb11f3a0acfb01135ef0376193
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-08 20:56:41 +0930

    Add JOKE ACT card

---
+++ DeltaruneActs/Cards/Joke.cs
@@ -0,0 +1,22 @@
+using DeltaruneActs.ACT;
+using DeltaruneActs.TP;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace DeltaruneActs.Cards;
+
+public sealed class Joke : DeltaruneActsCard
+{
+    public Joke() : base(0, CardType.Skill, CardRarity.Common, TargetType.AnyEnemy) { }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Target is not { IsDead: false } target || !TPManager.Spend(15))
+            return;
+
+        ActResolver.AddProgress(target, 10);
+        await BlockCmd.Gain(5, this);
+        TPManager.Gain(5);
+    }
+}

b53d66abce165055a4b264e851b4a21c7b67dcd7
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-10 08:32:04 +0930

    Fix exported card art paths and map action artwork

--- DeltaruneActs/Cards/DeltaruneActsCard.cs
+++ DeltaruneActs/Cards/DeltaruneActsCard.cs
@@ -10,9 +10,39 @@ namespace DeltaruneActs.Cards;
 public abstract class DeltaruneActsCard(int cost, CardType type, CardRarity rarity, TargetType target)
     : CustomCardModel(cost, type, rarity, target)
 {
-    private static string CardName(string entry) => entry.RemovePrefix().ToLowerInvariant();
+    private const string ArtRoot = "res://DeltaruneActs/images/card_portraits/";
 
-    public override string CustomPortraitPath => $"res://DeltaruneActs/images/card_portraits/big/{CardName(Id.Entry)}.png";
-    public override string PortraitPath => $"res://DeltaruneActs/images/card_portraits/{CardName(Id.Entry)}.png";
+    private string ArtName => GetType().Name switch
+    {
+        nameof(Check) => "kris_check",
+        nameof(Compliment) => "kris_compliment",
+        nameof(Warn) => "kris_social",
+        nameof(Joke) => "kris_social",
+        nameof(Flirt) => "kris_compliment",
+        nameof(Spare) => "kris_spare",
+        nameof(HealPrayer) => "ralsei_healprayer",
+        nameof(Healing) => "ralsei_heal",
+        nameof(OkayHeal) => "ralsei_heal",
+        nameof(BetterHeal) => "ralsei_heal",
+        nameof(UltraHeal) => "ralsei_heal",
+        nameof(UltimateHeal) => "ralsei_heal",
+        nameof(DualHeal) => "dual",
+        nameof(RudeBuster) => "susie_rudebuster",
+        nameof(RedBuster) => "susie_redbuster",
+        nameof(RudeSword) => "susie_rudebuster",
+        nameof(Scythemare) => "susie_rudebuster",
+        nameof(IceShock) => "kris_magic",
+        nameof(SnowGrave) => "kris_magic",
+        nameof(DualBuster) => "dual",
+        nameof(LightUp) => "ralsei_healprayer",
+        nameof(WakeKris) => "kris_magic",
+        nameof(ReviveKris) => "kris_magic",
+        nameof(ReviveSong) => "ralsei_healprayer",
+        nameof(SleepMist) => "ralsei_healprayer",
+        _ => "kris_social"
+    };
+
+    public override string CustomPortraitPath => $"{ArtRoot}{ArtName}.svg";
+    public override string PortraitPath => $"{ArtRoot}{ArtName}.svg";
     public override string BetaPortraitPath => PortraitPath;
 }

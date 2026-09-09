682f042122c69f40997fb16d4cca44d1353c3252
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-09 20:54:38 +0930

    Update Compliment for STS2 v0.111 API

--- DeltaruneActs/Cards/Compliment.cs
+++ DeltaruneActs/Cards/Compliment.cs
@@ -3,6 +3,7 @@ using DeltaruneActs.TP;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace DeltaruneActs.Cards;
 
@@ -16,6 +17,6 @@ public sealed class Compliment : DeltaruneActsCard
             return;
 
         ActResolver.AddProgress(target, 20);
-        await BlockCmd.Gain(5, this);
+        await CreatureCmd.GainBlock(Owner.Creature, 5, ValueProp.Move, cardPlay);
     }
 }

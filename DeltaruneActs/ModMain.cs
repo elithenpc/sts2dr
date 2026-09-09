4770068a37fcb7c02f3e4bc7c51752893c55f7e5
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-10 08:07:09 +0930

    Fix duplicate card model registration

--- DeltaruneActs/ModMain.cs
+++ DeltaruneActs/ModMain.cs
@@ -3,7 +3,6 @@ using Godot;
 using HarmonyLib;
 using DeltaruneActs.TP;
 using MegaCrit.Sts2.Core.Modding;
-using MegaCrit.Sts2.Core.Models;
 
 namespace DeltaruneActs;
 
@@ -20,34 +19,6 @@ public partial class ModMain : Node
         var harmony = new Harmony(ModId);
         harmony.PatchAll(assembly);
         TPHooks.Install(harmony);
-
-        ModelDb.Inject(typeof(Cards.Check));
-        ModelDb.Inject(typeof(Cards.Compliment));
-        ModelDb.Inject(typeof(Cards.Warn));
-        ModelDb.Inject(typeof(Cards.Joke));
-        ModelDb.Inject(typeof(Cards.Flirt));
-        ModelDb.Inject(typeof(Cards.Spare));
-
-        ModelDb.Inject(typeof(Cards.HealPrayer));
-        ModelDb.Inject(typeof(Cards.Pacify));
-        ModelDb.Inject(typeof(Cards.RudeBuster));
-        ModelDb.Inject(typeof(Cards.RedBuster));
-        ModelDb.Inject(typeof(Cards.IceShock));
-        ModelDb.Inject(typeof(Cards.SnowGrave));
-        ModelDb.Inject(typeof(Cards.DualHeal));
-        ModelDb.Inject(typeof(Cards.ReviveSong));
-        ModelDb.Inject(typeof(Cards.SleepMist));
-        ModelDb.Inject(typeof(Cards.Scythemare));
-        ModelDb.Inject(typeof(Cards.RudeSword));
-        ModelDb.Inject(typeof(Cards.Healing));
-        ModelDb.Inject(typeof(Cards.OkayHeal));
-        ModelDb.Inject(typeof(Cards.BetterHeal));
-        ModelDb.Inject(typeof(Cards.UltraHeal));
-        ModelDb.Inject(typeof(Cards.UltimateHeal));
-        ModelDb.Inject(typeof(Cards.DualBuster));
-        ModelDb.Inject(typeof(Cards.LightUp));
-        ModelDb.Inject(typeof(Cards.WakeKris));
-        ModelDb.Inject(typeof(Cards.ReviveKris));
 
         Logger.Info("Deltarune Acts loaded: ACT + TP + full spell set online.");
     }

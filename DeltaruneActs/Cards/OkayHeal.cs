d00af1d579fde7c7f4294bbd3639d203354b9a1a
elithenpc <163964890+elithenpc@users.noreply.github.com>
2026-09-11 11:36:57 +0930

    Make OkayHeal self-targeting

--- DeltaruneActs/Cards/OkayHeal.cs
+++ DeltaruneActs/Cards/OkayHeal.cs
@@ -1,17 +1,19 @@
 using DeltaruneActs.TP;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
 namespace DeltaruneActs.Cards;
 
 public sealed class OkayHeal : DeltaruneActsCard
 {
-    public OkayHeal() : base(0, CardType.Skill, CardRarity.Uncommon, TargetType.AnyAlly) { }
+    public OkayHeal() : base(0, CardType.Skill, CardRarity.Uncommon, TargetType.Self) { }
+
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        if (cardPlay.Target is not Creature target || target.IsDead || !TPManager.Spend(50)) return;
-        await CreatureCmd.Heal(target, 20);
+        if (!TPManager.Spend(50))
+            return;
+
+        await CreatureCmd.Heal(Owner.Creature, 20);
     }
 }
